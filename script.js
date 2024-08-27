2f8ef340a1d45d7f848025951e14712e2291ab18
peter <112580184+petyahyria@users.noreply.github.com>
2024-08-27 18:29:41 +0300

    Add feature turn(), activePlayer, getPlayerInput()
    activePlayer - player that currently makes turn
    turn() - feature of making turn, getPlayerInput() and add activePlayer
    .marker to the gameboard, change activePlayer()

--- script.js
+++ script.js
@@ -10,9 +10,39 @@ function createPlayer(name, marker){
     return {name, marker, score};
 }
 
-function playGame(player1, player2){
-    return {gameboard, player1, player2}
+function playGame(){
+    const player1 = createPlayer("p1", "X");
+    const player2 = createPlayer("p2", "O");
+    const gameboard = createGameboard();
+    console.table(gameboard.gameboard);
+    
+    const activePlayer = player1;
+
+    const getPlayerInput = () =>{
+        const row = prompt("Enter row (0, 1, 2):");
+        const column = prompt("Enter column (0, 1, 2):");
+        return [+row, +column];
+    }
+
+    const changeActivePlayer = () =>{
+        activePlayer === player1 ? player2 : player1;
+    }
+
+    const turn = () =>{
+        const [row, column] = getPlayerInput();
+        
+        gameboard.gameboard[row][column] += activePlayer.marker;
+        changeActivePlayer();
+        console.table(gameboard.gameboard);
+        console.log(activePlayer.name);
+    }
+
+    turn();
+
+    
 }
+
+playGame();
 
 
 
